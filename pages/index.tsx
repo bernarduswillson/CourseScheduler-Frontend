135cefbf764fd1f4c69e8e9a42f2a85d98f9078d
bewe <16521486@mahasiswa.itb.ac.id>
2023-08-06 21:28:36 +0700

    feat: done main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,63 +61,101 @@ const LandingPage: React.FC = () => {
 
   return (
     <div className='h-auto relative'>
-      <a href="/CRUD">
+      <div className='h-2'></div>
+      <div className='text-center font-bold text-[40px]'>Course Scheduler</div>
+      <a href="/CRUD" className='bg-[#c9deff] rounded-lg p-2 ml-10 hover:opacity-80'>
         <button>CRUD</button>
       </a>
-      <div className='flex'>
-        <ul className='w-1/2'>
+      <div className='flex mt-2 ml-10'>
+        <ul className='w-[35%]'>
+          <div className='flex text-white text-center'>
+            <div className='w-[40%] bg-gray-700 border-[1px]'>Mata Kuliah</div>
+            <div className='w-[10%] bg-gray-700 border-[1px]'>SKS</div>
+            <div className='w-[13%] bg-gray-700 border-[1px]'>Jurusan</div>
+            <div className='w-[15%] bg-gray-700 border-[1px]'>Minimal Semester</div>
+            <div className='w-[15%] bg-gray-700 border-[1px]'>Prediksi Indeks</div>
+          </div>
           {data.length > 0
             ? (
                 data.map((item) => (
-                <li key={item.id}>
-                  {item.nama_mk} - {item.sks} SKS - {item.jurusan_mk} - {item.semester_minimal} - {item.prediksi}
+                <li key={item.id} className='flex'>
+                  <div className='w-[40%] border-2 p-1'>{item.nama_mk}</div>
+                  <div className='w-[10%] border-2 p-1'>{item.sks}</div>
+                  <div className='w-[13%] border-2 p-1'>{item.jurusan_mk}</div>
+                  <div className='w-[15%] border-2 p-1'>{item.semester_minimal}</div>
+                  <div className='w-[15%] border-2 p-1'>{item.prediksi}</div>
                 </li>
                 ))
               )
             : (
-            <li>No data available</li>
+              <div className='font-bold bg-red-500 w-[80%]'>No data available</div>
               )}
         </ul>
-        <div className='w-1/2'>
+        <div className='w-[65%]'>
+          <div className='flex font-bold'>
+            <div className='w-[20%] border-2 p-1'>Jurusan</div>
+            <div className='w-[20%] border-2 p-1'>Semester Pengambilan</div>
+            <div className='w-[20%] border-2 p-1'>SKS Minimal</div>
+            <div className='w-[20%] border-2 p-1'>SKS Maksimal</div>
+          </div>
           <div>
             <input
               type='text'
               value={userData.jurusan}
               onChange={(e) => setUserData({ ...userData, jurusan: e.target.value })}
               placeholder='Jurusan'
+              className='w-[20%]'
             />
             <input
               type='number'
               value={userData.semester_ambil}
               onChange={(e) => setUserData({ ...userData, semester_ambil: parseInt(e.target.value) })}
               placeholder='Semester Pengambilan'
+              className='w-[20%]'
             />
             <input
               type='number'
               value={userData.sks_minimal}
               onChange={(e) => setUserData({ ...userData, sks_minimal: parseInt(e.target.value) })}
               placeholder='SKS Minimal'
+              className='w-[20%]'
             />
             <input
               type='number'
               value={userData.sks_maksimal}
               onChange={(e) => setUserData({ ...userData, sks_maksimal: parseInt(e.target.value) })}
               placeholder='SKS Maksimal'
+              className='w-[20%]'
             />
 
-            <button onClick={handlePostUserData}>Post User Data</button>
+            <button onClick={handlePostUserData} className='bg-[#5c1717] text-white p-2 rounded-lg ml-2 hover:opacity-80'>Search</button>
           </div>
           <div>
-          <h2>Selected Courses:</h2>
-            <ul>
-              {selectedCourses.map((course) => (
-                <li key={course.id}>
-                  {course.nama_mk} - {course.sks} SKS - {course.jurusan_mk} - {course.semester_minimal} - {course.prediksi}
-                </li>
-              ))}
-            </ul>
-            <h2>Total Selected Score: {totalSelectedScore}</h2>
-            <h2>Total Selected SKS: {totalSelectedSKS}</h2>
+            <h2>Selected Courses:</h2>
+              <div className='flex text-white text-center'>
+                <div className='w-[40%] bg-gray-700 border-[1px]'>Mata Kuliah</div>
+                <div className='w-[10%] bg-gray-700 border-[1px]'>SKS</div>
+                <div className='w-[10%] bg-gray-700 border-[1px]'>Jurusan</div>
+                <div className='w-[10%] bg-gray-700 border-[1px]'>Minimal Semester</div>
+                <div className='w-[10%] bg-gray-700 border-[1px]'>Prediksi Indeks</div>
+              </div>
+              {selectedCourses.length > 0
+                ? (
+                    selectedCourses.map((course) => (
+                    <li key={course.id} className='flex'>
+                      <div className='w-[40%] border-2 p-1'>{course.nama_mk}</div>
+                      <div className='w-[10%] border-2 p-1'>{course.sks}</div>
+                      <div className='w-[10%] border-2 p-1'>{course.jurusan_mk}</div>
+                      <div className='w-[10%] border-2 p-1'>{course.semester_minimal}</div>
+                      <div className='w-[10%] border-2 p-1'>{course.prediksi}</div>
+                    </li>
+                    ))
+                  )
+                : (
+                <div className='font-bold bg-red-500 w-[80%]'>No course available</div>
+                  )}
+            <h2>Prediksi IP: <span className='font-bold'>{totalSelectedScore.toFixed(2)}</span></h2>
+            <h2>Jumlah SKS: <span className='font-bold'>{totalSelectedSKS}</span></h2>
           </div>
         </div>
       </div>
